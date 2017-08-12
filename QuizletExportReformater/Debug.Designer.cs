3f19e93b53a9db235845b7fa3bfcc6af1738ed07
kilpikonna <871674895@qq.com>
2017-08-12 22:41:21 +0800

    Revert "Revert "添加项目文件。""
    This reverts commit 8b211536b3229229f60518c4af720e85cca59034.

---
+++ QuizletExportReformater/Debug.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace QuizletExportReformater
+{
+    partial class Debug
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextB = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // TextB
+            // 
+            this.TextB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextB.Location = new System.Drawing.Point(0, 0);
+            this.TextB.Name = "TextB";
+            this.TextB.Size = new System.Drawing.Size(1147, 595);
+            this.TextB.TabIndex = 0;
+            this.TextB.Text = "";
+            // 
+            // Debug
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 598);
+            this.Controls.Add(this.TextB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Debug";
+            this.Text = "Debug";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Debug_FormClosing);
+            this.Load += new System.EventHandler(this.Debug_Load);
+            this.ClientSizeChanged += new System.EventHandler(this.Debug_ClientSizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextB;
+    }
+}
